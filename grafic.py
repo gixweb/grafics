43d517dca59bc8462f1c907e17286e7d4b6e07b6
Giselly <153649835+gixweb@users.noreply.github.com>
2025-09-02 00:04:06 +0000

    reparos no code

--- grafic.py
+++ grafic.py
@@ -2,13 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv(r'C:\Users\gisel\OneDrive\Documentos\codesDocs\StressLevelDataset.csv')
-
-print(df.columns)
+df = pd.read_csv('StressLevelDataset.csv')
 
 # Criar o histograma de barras
 plt.figure(figsize=(10, 6))
-sns.histplot(data=df, x='stress level', bins=10, kde=False)
+sns.histplot(data=df, x='stress_level', bins=10, kde=False)
 plt.title('Histograma da Distribuição do Nível de Estresse')
 plt.xlabel('Nível de Estresse')
 plt.ylabel('Frequência')
@@ -17,7 +15,7 @@ plt.show()
 
 # Criar a curva de densidade de dados
 plt.figure(figsize=(10, 6))
-sns.kdeplot(data=df, x='stress level', fill=True, color='purple')
+sns.kdeplot(data=df, x='stress_level', fill=True, color='purple')
 plt.title('Curva de Densidade do Nível de Estresse')
 plt.xlabel('Nível de Estresse')
 plt.ylabel('Densidade')
@@ -25,24 +23,24 @@ plt.show()
 
 # Criar o boxplot com outliers
 plt.figure(figsize=(10, 6))
-sns.boxplot(data=df, y='stress level')
+sns.boxplot(data=df, y='stress_level')
 plt.title('Boxplot do Nível de Estresse (com Outliers)')
 plt.ylabel('Nível de Estresse')
 plt.show()
 
 # Calcular o IQR e os limites dos outliers
-Q1 = df['stress level'].quantile(0.25)
-Q3 = df['stress level'].quantile(0.75)
+Q1 = df['stress_level'].quantile(0.25)
+Q3 = df['stress_level'].quantile(0.75)
 IQR = Q3 - Q1
 lower_bound = Q1 - 1.5 * IQR
 upper_bound = Q3 + 1.5 * IQR
 
 # Criar um novo DataFrame sem os outliers
-df_no_outliers = df[(df['stress level'] >= lower_bound) & (df['stress level'] <= upper_bound)]
+df_no_outliers = df[(df['stress_level'] >= lower_bound) & (df['stress_level'] <= upper_bound)]
 
 # Criar o boxplot sem outliers
 plt.figure(figsize=(10, 6))
-sns.boxplot(data=df_no_outliers, y='stress level')
+sns.boxplot(data=df_no_outliers, y='stress_level')
 plt.title('Boxplot do Nível de Estresse (sem Outliers)')
 plt.ylabel('Nível de Estresse')
 plt.show()
